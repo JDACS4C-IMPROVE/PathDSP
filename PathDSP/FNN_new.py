a5e4360eb2ed8aa0ccfd1d13729b1cb0e7fa9ded
Liuy12 <yuanhangliu.ok@gmail.com>
2023-08-16 13:29:22 -0700

    update FNN_new

--- PathDSP/FNN_new.py
+++ PathDSP/FNN_new.py
@@ -45,6 +45,15 @@ class RMSELoss(tch.nn.Module):
         loss = tch.sqrt(criterion(x, y) + eps)
         return loss
 
+
+def r2_score(y_true, y_pred):
+    y_mean = tch.mean(y_true)
+    ss_tot = tch.sum((y_true - y_mean)**2)
+    ss_res = tch.sum((y_true - y_pred)**2)
+    r2 = 1 - ss_res / ss_tot
+    return r2
+
+
 def fit(net, train_dl, valid_dl, epochs, learning_rate, device, opt_fn):
     """
     Return train and valid performance including loss
@@ -64,6 +73,7 @@ def fit(net, train_dl, valid_dl, epochs, learning_rate, device, opt_fn):
     net = net.to(device) # load the network onto the device
     trainloss_list = [] # metrics: MSE, size equals to EPOCH
     validloss_list = [] # metrics: MSE, size equals to EPOCH
+    validr2_list = [] # metrics: r2, size equals to EPOCH
     early_stopping = myutil.EarlyStopping(patience=30, verbose=True) # initialize the early_stopping
     # repeat the training for EPOCH times
     for epoch in range(epochs):
@@ -90,15 +100,21 @@ def fit(net, train_dl, valid_dl, epochs, learning_rate, device, opt_fn):
         with tch.no_grad():
             net.eval()
             valid_epoch_loss = 0.0 # save loss for each epoch, batch by batch
+            ss_res = 0.0
+            ss_tot = 0.0
             for i, (X_valid, y_valid) in enumerate(valid_dl):
                 X_valid, y_valid = X_valid.to(device), y_valid.to(device) # load data onto the device
                 y_valid_pred  = net(X_valid) # valid result
                 valid_loss = criterion(y_valid_pred, y_valid.float())#y_valid.unsqueeze(1)) # calculate loss
                 valid_epoch_loss += valid_loss.item() # adding loss from each batch
+                ss_res += tch.sum((y_valid_pred - y_valid.float())**2)
+                ss_tot += tch.sum((y_valid_pred - y_valid.mean())**2)
+        
         # calculate total loss of all batches, and append to result list
         avg_valid_loss = valid_epoch_loss / len(valid_dl)
         validloss_list.append( avg_valid_loss)
-
+        valid_r2 = 1 - ss_res / ss_tot
+        validr2_list.append(valid_r2)
         # display print message
         #print('epoch={:}/{:}, train loss={:.5f}, valid loss={:.5f}'.format(
         #       epoch+1, epochs, train_epoch_loss / len(train_dl),
@@ -115,7 +131,7 @@ def fit(net, train_dl, valid_dl, epochs, learning_rate, device, opt_fn):
     # load the last checkpoint with the best model
     net.load_state_dict(tch.load('checkpoint.pt'))
 
-    return  net, trainloss_list, validloss_list
+    return  net, trainloss_list, validloss_list, validr2_list
 
 def predict(net, test_dl, device):
     """
@@ -150,121 +166,128 @@ def main(params):
     # load data
     train_df = pd.read_csv(params['train_data'], header=0, index_col=[0,1], sep="\t")
     val_df = pd.read_csv(params['val_data'], header=0, index_col=[0,1], sep="\t")
+    test_df = pd.read_csv(params['test_data'], header=0, index_col=[0,1], sep="\t")
     
     # shuffle
-    sdf = skut.shuffle(df, random_state=params["seed_int"])
+    #sdf = skut.shuffle(df, random_state=params["seed_int"])
 
     # set parameters
     myutil.set_seed(params["seed_int"])
     device = myutil.get_device(uth=params["gpu_int"])
-    kFold = params["cv_int"]
+    #kFold = params["cv_int"]
     learning_rate = params['learning_rate']
     epoch = params['epochs']
     batch_size = params['batch_size']
     opt_fn = tch.optim.Adam
 
     # create result list
-    loss_df_list = []
-    score_df_list = []
-    ytest_df_list = []
-    shap_df_list = []
-    # train with cross-validation
-    kf = skms.KFold(n_splits=kFold, random_state=params['seed_int'], shuffle=True)
-    X_df = sdf.iloc[:, 0:-1]
-    y_df = sdf.iloc[:, -1]
+    # loss_df_list = []
+    # score_df_list = []
+    # ytest_df_list = []
+    # shap_df_list = []
+    # # train with cross-validation
+    #kf = skms.KFold(n_splits=kFold, random_state=params['seed_int'], shuffle=True)
+    #X_df = train_df.iloc[:, 0:-1]
+    #y_df = train_df.iloc[:, -1]
     # save best model with lowest RMSE
-    best_rmse = 10000
-    best_model = None
-    best_fold = 0
-    for i, (train_index, test_index) in enumerate(kf.split(X_df, y_df)):
-        n_fold = i+1
-        print('Fold={:}/{:}'.format(n_fold, params['cv_int']))
-        # get train/test splits
-        Xtrain_arr = X_df.values[train_index]
-        Xtest_arr = X_df.values[test_index]
-        ytrain_arr = y_df.values[train_index]
-        ytest_arr = y_df.values[test_index]
-        # get train/valid splits from train
-        Xtrain_arr, Xvalid_arr, ytrain_arr, yvalid_arr = skms.train_test_split(Xtrain_arr, ytrain_arr,
-                                                                               test_size=0.1, random_state=params['seed_int'])
-        print('    train={:}, valid={:}, test={:}'.format(Xtrain_arr.shape, Xvalid_arr.shape, Xtest_arr.shape))
-        # prepare dataframe for output
-        ytest_df = y_df.iloc[test_index].to_frame()
-        # convert to numpy array
-        Xtrain_arr = np.array(Xtrain_arr).astype('float32')
-        Xvalid_arr = np.array(Xvalid_arr).astype('float32')
-        Xtest_arr = np.array(Xtest_arr).astype('float32')
-        ytrain_arr = np.array(ytrain_arr).astype('float32')
-        yvalid_arr = np.array(yvalid_arr).astype('float32')
-        ytest_arr = np.array(ytest_arr).astype('float32')
-        # create mini-batch
-        train_dataset = mydl.NumpyDataset(tch.from_numpy(Xtrain_arr), tch.from_numpy(ytrain_arr))
-        valid_dataset = mydl.NumpyDataset(tch.from_numpy(Xvalid_arr), tch.from_numpy(yvalid_arr))
-        test_dataset = mydl.NumpyDataset(tch.from_numpy(Xtest_arr), tch.from_numpy(ytest_arr))
-        train_dl = tchud.DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-        valid_dl = tchud.DataLoader(valid_dataset, batch_size=batch_size, shuffle=False)
-        test_dl = tchud.DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
-        # initial weight
-        def init_weights(m):
-            if type(m) == tch.nn.Linear:
-                tch.nn.init.kaiming_uniform_(m.weight)
-                m.bias.data.fill_(0.01)
-        # load model
-        n_features = Xtrain_arr.shape[1]
-        net = mynet.FNN(n_features)
-        net.apply(init_weights)
-        # fit data with model
-        trained_net, train_loss_list, valid_loss_list = fit(net, train_dl, valid_dl, epoch, learning_rate, device, opt_fn)
-        prediction_list = predict(trained_net, test_dl, device)
-        # evaluation metrics
-        mse = skmts.mean_squared_error(ytest_arr, prediction_list)
-        rmse = np.sqrt(mse)
-        if rmse <= best_rmse:
-            best_rmse = rmse
-            best_fold = n_fold
-            best_model = trained_net
-            print('best model so far at fold={:}, rmse={:}'.format(best_fold, best_rmse))
+#     best_rmse = 10000
+#     best_model = None
+#     best_fold = 0
+# #    for i, (train_index, test_index) in enumerate(kf.split(X_df, y_df)):
+    #n_fold = i+1
+    #print('Fold={:}/{:}'.format(n_fold, params['cv_int']))
+    # get train/test splits
+    Xtrain_arr = train_df.iloc[:, 0:-1].values
+    Xvalid_arr = val_df.iloc[:, 0:-1].values
+    Xtest_arr = test_df.iloc[:, 0:-1].values
+    ytrain_arr = train_df.iloc[:, -1].values
+    yvalid_arr = val_df.iloc[:, -1].values    
+    ytest_arr = test_df.iloc[:, -1].values
+    
+    # get train/valid splits from train
+    #Xtrain_arr, Xvalid_arr, ytrain_arr, yvalid_arr = skms.train_test_split(Xtrain_arr, ytrain_arr,
+    #                                                                        test_size=0.1, random_state=params['seed_int'])
+    #print('    train={:}, valid={:}, test={:}'.format(Xtrain_arr.shape, Xvalid_arr.shape, Xtest_arr.shape))
+    # prepare dataframe for output
+    ytest_df = test_df.iloc[:, -1].to_frame()
+    # convert to numpy array
+    Xtrain_arr = np.array(Xtrain_arr).astype('float32')
+    Xvalid_arr = np.array(Xvalid_arr).astype('float32')
+    Xtest_arr = np.array(Xtest_arr).astype('float32')
+    ytrain_arr = np.array(ytrain_arr).astype('float32')
+    yvalid_arr = np.array(yvalid_arr).astype('float32')
+    ytest_arr = np.array(ytest_arr).astype('float32')
+    # create mini-batch
+    train_dataset = mydl.NumpyDataset(tch.from_numpy(Xtrain_arr), tch.from_numpy(ytrain_arr))
+    valid_dataset = mydl.NumpyDataset(tch.from_numpy(Xvalid_arr), tch.from_numpy(yvalid_arr))
+    test_dataset = mydl.NumpyDataset(tch.from_numpy(Xtest_arr), tch.from_numpy(ytest_arr))
+    train_dl = tchud.DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    valid_dl = tchud.DataLoader(valid_dataset, batch_size=batch_size, shuffle=False)
+    test_dl = tchud.DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+    # initial weight
+    def init_weights(m):
+        if type(m) == tch.nn.Linear:
+            tch.nn.init.kaiming_uniform_(m.weight)
+            m.bias.data.fill_(0.01)
+    # load model
+    n_features = Xtrain_arr.shape[1]
+    net = mynet.FNN(n_features)
+    net.apply(init_weights)
+    # fit data with model
+    trained_net, train_loss_list, valid_loss_list, valid_r2_list = fit(net, train_dl, valid_dl, epoch, learning_rate, device, opt_fn)
+    prediction_list = predict(trained_net, test_dl, device)
+    # evaluation metrics
+    mse = skmts.mean_squared_error(ytest_arr, prediction_list)
+    rmse = np.sqrt(mse)
+    r2_pred = r2_score(ytest_arr, prediction_list)
+    loss_pred = pd.DataFrame({'rmse': rmse,
+                              'r2': r2_pred})
+    loss_pred.to_csv(params['output'] + '/Loss_pred.txt', header=True, index=False, sep="\t")
+    # if rmse <= best_rmse:
+    #     best_rmse = rmse
+    #     best_fold = n_fold
+    #     best_model = trained_net
+    #     print('best model so far at fold={:}, rmse={:}'.format(best_fold, best_rmse))
+    
+    
+    # if params['shap_bool'] == True:
+    #     print('calculate shapely values')
+    #     # random select 100 samples as baseline
+    #     train_dataset = mydl.NumpyDataset(tch.from_numpy(Xtrain_arr), tch.from_numpy(ytrain_arr))
+    #     train_dl = tchud.DataLoader(train_dataset, batch_size=200, shuffle=True)
+    #     background, lbl  = next(iter(train_dl))
+    #     explainer = sp.DeepExplainer(trained_net, background[:100].to(device))
+    #     shap_arr = explainer.shap_values(tch.from_numpy(Xtest_arr))
+    #     shap_df = pd.DataFrame(shap_arr, index=ytest_df.index, columns=X_df.columns)
+    #     # append to result
+    #     shap_df_list.append(shap_df)
         
-     
-        if params['shap_bool'] == True:
-            print('calculate shapely values')
-            # random select 100 samples as baseline
-            train_dataset = mydl.NumpyDataset(tch.from_numpy(Xtrain_arr), tch.from_numpy(ytrain_arr))
-            train_dl = tchud.DataLoader(train_dataset, batch_size=200, shuffle=True)
-            background, lbl  = next(iter(train_dl))
-            explainer = sp.DeepExplainer(trained_net, background[:100].to(device))
-            shap_arr = explainer.shap_values(tch.from_numpy(Xtest_arr))
-            shap_df = pd.DataFrame(shap_arr, index=ytest_df.index, columns=X_df.columns)
-            # append to result
-            shap_df_list.append(shap_df)
-            
-        # collect result
-        loss_df = pd.DataFrame({'fold':[n_fold]*len(train_loss_list), 
-                                'epoch':[i+1 for i in range(len(train_loss_list))],
-                                'train loss':train_loss_list, 
-                                'valid loss': valid_loss_list})
-        ytest_df['prediction'] = prediction_list
-        ytest_df['fold'] = n_fold
-        loss_df_list.append(loss_df)
-        ytest_df_list.append(ytest_df)
-        # end of fold
-        trained_net = None
-        break
+    # collect result
+    loss_df = pd.DataFrame({'epoch':[i+1 for i in range(len(train_loss_list))],
+                            'train loss':train_loss_list, 
+                            'valid loss': valid_loss_list,
+                            'valid r2': valid_r2_list})
+    ytest_df['prediction'] = prediction_list
+    #loss_df_list.append(loss_df)
+    #ytest_df_list.append(ytest_df)
+    # end of fold
+    #trained_net = None
 
     # save to output
-    all_ytest_df = pd.concat(ytest_df_list, axis=0)
-    all_loss_df = pd.concat(loss_df_list, axis=0)
-    all_ytest_df.to_csv(params['output'] + '.FNN.cv_' + str(params['cv_int']) + '.Prediction.txt', header=True, index=True, sep="\t")
-    all_loss_df.to_csv(params['output'] + '.FNN.cv_' + str(params['cv_int']) + '.Loss.txt', header=True, index=False, sep="\t")
-    if params['shap_bool'] == True:
-        all_shap_df = pd.concat(shap_df_list, axis=0)
-        all_shap_df.to_csv(params['output'] + '.FNN.cv_' + str(params['cv_int']) + '.SHAP.txt', header=True, index=True, sep="\t")
+    #all_ytest_df = pd.concat(ytest_df_list, axis=0)
+    #all_loss_df = pd.concat(loss_df_list, axis=0)
+    ytest_df.to_csv(params['output'] + '/Prediction.txt', header=True, index=True, sep="\t")
+    loss_df.to_csv(params['output'] + '/Loss.txt', header=True, index=False, sep="\t")
+    # if params['shap_bool'] == True:
+    #     all_shap_df = pd.concat(shap_df_list, axis=0)
+    #     all_shap_df.to_csv(params['output'] + '.FNN.cv_' + str(params['cv_int']) + '.SHAP.txt', header=True, index=True, sep="\t")
 
     # make train/valid loss plots
-    tch.save(best_model.state_dict(), params['output'] + '.FNN.cv_' + str(params['cv_int']) + 'best_model.pt')
+    best_model = trained_net
+    tch.save(best_model.state_dict(), params['output'] + '/model.pt')
     print( '[Finished in {:}]'.format(myutil.cal_time(datetime.now(), start_time)) )
     # display evaluation metrics of all folds
-    mse, rmse, r_square, pccy = mymts.eval_regressor_performance(all_ytest_df, 'response', 'prediction')
+    #mse, rmse, r_square, pccy = mymts.eval_regressor_performance(all_ytest_df, 'response', 'prediction')
 
 
 
