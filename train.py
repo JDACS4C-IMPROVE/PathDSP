e6bd11573c754952d3f87bbcc581795d04618ed4
Liuy12 <yuanhangliu.ok@gmail.com>
2023-08-17 11:14:45 -0700

    update train.sh

--- train.py
+++ train.py
@@ -1,13 +1,13 @@
 import candle
 import os
-import json
-from json import JSONEncoder
+#import json
+#from json import JSONEncoder
 from preprocess_new import mkdir, preprocess
 from PathDSP.FNN_new import main
 
 file_path = os.path.dirname(os.path.realpath(__file__))
 # This should be set outside as a user environment variable
-os.environ['CANDLE_DATA_DIR'] = os.environ['HOME'] + '/improve_data_dir/'
+#os.environ['CANDLE_DATA_DIR'] = os.environ['HOME'] + '/improve_data_dir/'
 required = None
 additional_definitions = None
 
@@ -35,34 +35,36 @@ def initialize_parameters():
     gParameters = candle.finalize_parameters(preprocessor_bmk)
     return gParameters
 
-class CustomData:
-    def __init__(self, name, value):
-        self.name = name
-        self.value = value
+# class CustomData:
+#     def __init__(self, name, value):
+#         self.name = name
+#         self.value = value
 
-class CustomEncoder(json.JSONEncoder):
-    def default(self, o):
-            return o.__dict__
+# class CustomEncoder(json.JSONEncoder):
+#     def default(self, o):
+#             return o.__dict__
 
 
-def run(params):
-    params['data_type'] = str(params['data_type'])
-    json_out = params['output_dir']+'/params.json'
-    print(params)
+# def run(params):
+#     params['data_type'] = str(params['data_type'])
+#     json_out = params['output_dir']+'/params.json'
+#     print(params)
 
-    with open (json_out, 'w') as fp:
-        json.dump(params, fp, indent=4, cls=CustomEncoder)
+#     with open (json_out, 'w') as fp:
+#         json.dump(params, fp, indent=4, cls=CustomEncoder)
 
-    scores = main(params)
-    with open(params['output_dir'] + "/scores.json", "w", encoding="utf-8") as f:
-        json.dump(scores, f, ensure_ascii=False, indent=4)
-#    print('IMPROVE_RESULT RMSE:\t' + str(scores['rmse']))
+#     scores = main(params)
+#     with open(params['output_dir'] + "/scores.json", "w", encoding="utf-8") as f:
+#         json.dump(scores, f, ensure_ascii=False, indent=4)
+# #    print('IMPROVE_RESULT RMSE:\t' + str(scores['rmse']))
 
 
 def candle_main():
     params = initialize_parameters()
-    params =  preprocess(params)
-    run(params)
+    data_dir = os.environ['CANDLE_DATA_DIR'] + '/' + '/Data/'
+    params =  preprocess(params, data_dir)
+    main(params)
+
 
 if __name__ == "__main__":
     candle_main()
