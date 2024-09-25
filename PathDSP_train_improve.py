c4557e59cc9e41f04df266b433af43d8099439e8
nkoussa <156325369+nkoussa@users.noreply.github.com>
2024-09-25 16:10:20 -0400

    Update PathDSP_train_improve.py

--- PathDSP_train_improve.py
+++ PathDSP_train_improve.py
@@ -286,7 +286,8 @@ def run(params):
         y_pred=val_pred, 
         stage="val",
         y_col_name=params["y_col_name"],
-        output_dir=params["output_dir"]
+        output_dir=params["output_dir"],
+        input_dir=params["input_dir"]
     )
 
     # -----------------------------
