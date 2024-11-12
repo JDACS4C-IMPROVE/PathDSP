75b6dd8cd374a9ca2bfcd3ea19180de09493bd7e
nkoussa <156325369+nkoussa@users.noreply.github.com>
2024-11-12 09:41:23 -0500

    deephyper updates

---
+++ hpo_deephyper_params_def.py
@@ -0,0 +1,56 @@
+additional_definitions = [
+    {"name": "source",
+     "type": str,
+     "default": "GDSCv1",
+     "help": "source dataset for HPO"
+    },
+    {"name": "split",
+     "type": str,
+     "default": "4",
+     "help": "Split of the source datasets for HPO"
+    },
+    {"name": "model_name",
+     "type": str,
+     "default": 'PathDSP',
+     "help": "Name of the deep learning model"
+    },
+
+
+
+    {"name": "model_scripts_dir",
+     "type": str,
+     "default": './', 
+     "help": "Path to the model repository"
+    },
+    {"name": "model_environment",
+     "type": str,
+     "default": '',
+     "help": "Name of your model conda environment"
+    },
+    {"name": "hyperparameters_file",
+     "type": str,
+     "default": 'hyperparameters_default.json',
+     "help": "json file containing optimized hyperparameters per dataset"
+    },
+    {"name": "epochs",
+     "type": int,
+     "default": 10,
+     "help": "Number of epochs"
+    },
+    {"name": "available_accelerators",
+     "nargs" : "+",
+     "type": str,
+     "default": ["0", "1"],
+     "help": "GPU IDs to assign jobs"
+    },
+    {"name": "use_singularity",
+     "type": bool,
+     "default": True,
+     "help": "Do you want to use singularity image for running the model?"
+    },
+    {"name": "singularity_image",
+     "type": str,
+     "default": '',
+     "help": "Singularity image file of the model"
+    }
+    ]
---
+++ hpo_deephyper_subprocess_train.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+# bash subprocess_train.sh ml_data/CCLE-CCLE/split_0 ml_data/CCLE-CCLE/split_0 out_model/CCLE/split_0
+# CUDA_VISIBLE_DEVICES=5 bash subprocess_train.sh ml_data/CCLE-CCLE/split_0 ml_data/CCLE-CCLE/split_0 out_model/CCLE/split_0
+
+# Need to comment this when using ' eval "$(conda shell.bash hook)" '
+# set -e
+
+# Activate conda env for model using "conda activate myenv"
+# https://saturncloud.io/blog/activating-conda-environments-from-scripts-a-guide-for-data-scientists
+# https://stackoverflow.com/questions/34534513/calling-conda-source-activate-from-bash-script
+# This doesn't work w/o eval "$(conda shell.bash hook)"
+CONDA_ENV=$PathDSP_env
+#echo "Allow conda commands in shell script by running 'conda shell.bash hook'"
+#eval "$(conda shell.bash hook)"
+echo "Activated conda commands in shell script"
+#conda activate $CONDA_ENV
+#source activate $CONDA_ENV
+conda_path=$(dirname $(dirname $(which conda)))
+source $conda_path/bin/activate $CONDA_ENV
+#source /soft/datascience/conda/2023-10-04/mconda3/bin/activate $CONDA_ENV
+#source activate $CONDA_ENV
+echo "Activated conda env $CONDA_ENV"
+#model path, model name, epochs
+MODELPATH=$1
+MODELNAME=$2
+input_dir=$3
+output_dir=$4
+learning_rate=$5
+batch_size=$6
+epochs=$7
+#cuda_name=$6
+CUDA_VISIBLE_DEVICES=$8
+
+#echo "train_ml_data_dir: $train_ml_data_dir"
+#echo "val_ml_data_dir:   $val_ml_data_dir"
+echo "input_dir:      $input_dir"
+echo "output_dir:      $output_dir"
+echo "learning_rate:      $learning_rate"
+echo "batch_size:      $batch_size"
+#echo "cuda_name:       $cuda_name"
+echo "CUDA_VISIBLE_DEVICES: $CUDA_VISIBLE_DEVICES"
+
+# epochs=10
+#epochs=10
+# epochs=50
+
+# All train outputs are saved in params["model_outdir"]
+#CUDA_VISIBLE_DEVICES=6,7 python PathDSP_train_improve.py \
+#CUDA_VISIBLE_DEVICES=5
+#CUDA_VISIBLE_DEVICES=6,7
+CUDA_VISIBLE_DEVICES=${CUDA_VISIBLE_DEVICES} python $MODELPATH/$MODELNAME_train_improve.py \
+    --input_dir $input_dir \
+    --output_dir $output_dir \
+    --epochs $epochs \
+    --learning_rate $learning_rate \
+    --batch_size $batch_size
+#    --cuda_name $cuda_name
+
+#conda deactivate
+source $conda_path/bin/deactivate
+echo "Deactivated conda env $CONDA_ENV"
