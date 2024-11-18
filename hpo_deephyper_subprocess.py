3f999dfa0379b37c52de305a5f1ecb4fd2f163d0
nkoussa <156325369+nkoussa@users.noreply.github.com>
2024-11-18 12:08:03 -0500

    creates output dir

--- hpo_deephyper_subprocess.py
+++ hpo_deephyper_subprocess.py
@@ -135,7 +135,8 @@ if __name__ == "__main__":
         default_config="hpo_deephyper_params.ini",
         additional_definitions=hpo_deephyper_params_def.additional_definitions
     )
-
+    if params['output_dir'].exists() is False:
+        os.makedirs(params['output_dir'], exist_ok=True)
     params['ml_data_dir'] = f"ml_data/{params['source']}-{params['source']}/split_{params['split']}"
     params['model_outdir'] = f"{params['output_dir']}/{params['source']}/split_{params['split']}"
     params['script_name'] = os.path.join(params['model_scripts_dir'],f"{params['model_name']}_train_improve.py")
