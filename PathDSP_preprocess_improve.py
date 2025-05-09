b5d20309f5e99abe2d40ca96393038767bb93ce3
nkoussa <156325369+nkoussa@users.noreply.github.com>
2025-05-09 14:55:10 -0400

    bug

--- PathDSP_preprocess_improve.py
+++ PathDSP_preprocess_improve.py
@@ -190,7 +190,7 @@ def prep_input(params, response_df):
         comb_data_mtx = pd.DataFrame({params['drug_col_name']: response_df[params['drug_col_name']].values,
                                       params['canc_col_name']: response_df[params['canc_col_name']].values,
                                       "exp_id": response_df["exp_id"].values, 
-                                      params['y_col_name']: params['y_col_name'].values})
+                                      params['y_col_name']: response_df[params['y_col_name']].values})
         comb_data_mtx = (comb_data_mtx.set_index([params['drug_col_name'], params['canc_col_name'], "exp_id", params['y_col_name']]).join(drug_data, on=params['drug_col_name']).join(sample_data, on=params['canc_col_name']))
         ss = StandardScaler()
         comb_data_mtx.iloc[:,params["bit_int"]:comb_data_mtx.shape[1]] = ss.fit_transform(comb_data_mtx.iloc[:,params["bit_int"]:comb_data_mtx.shape[1]])
