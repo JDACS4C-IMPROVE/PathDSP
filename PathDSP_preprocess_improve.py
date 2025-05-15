ff1b76492a9d85f044d096ca88d9a7a350f44007
nkoussa <156325369+nkoussa@users.noreply.github.com>
2025-05-15 15:17:56 -0400

    bug

--- PathDSP_preprocess_improve.py
+++ PathDSP_preprocess_improve.py
@@ -37,8 +37,8 @@ def check_smiles_RDKit(smile_df, col_name):
             bad_smiles[idx] = row['smile']
         else:
             good_smiles[idx] = row['smile']
-    bad_smiles = pd.DataFrame.from_dict(bad_smiles, orient='index', columns=col_name)
-    good_smiles = pd.DataFrame.from_dict(good_smiles, orient='index', columns=col_name)
+    bad_smiles = pd.DataFrame.from_dict(bad_smiles, orient='index', columns=[col_name])
+    good_smiles = pd.DataFrame.from_dict(good_smiles, orient='index', columns=[col_name])
     print("bad smiles:", bad_smiles)
     print("good smiles:", good_smiles)
     return bad_smiles, good_smiles
